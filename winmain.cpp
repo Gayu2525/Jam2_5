1f4d7285246963435f5df4d3893b872f13347071
Chino Masato <56014502+ChinoMasato@users.noreply.github.com>
2023-07-07 18:44:24 +0900

    ログ無効

--- winmain.cpp
+++ winmain.cpp
@@ -3,6 +3,7 @@
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	SetOutApplicationLogValidFlag(FALSE);//ログ無効
 	//DXライブラリの初期化処理 ここから
 	ChangeWindowMode(TRUE);	//ウィンドウモードにする
 	SetGraphMode(800, 600, 32);	//ウィンドウサイズを設定する
@@ -11,24 +12,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return -1;			// エラーが起きたら直ちに終了
 	}
 	SetDrawScreen(DX_SCREEN_BACK);	//裏画面を描画対象へ
-	//DXライブラリの初期化処理 ここまで
-
-
-	//ドット描画処理
-	int color = GetColor(255, 255, 255);
-	DrawPixel(400, 300, color);
-	ScreenFlip();		//裏画面と表画面の入替
 
 
 	//メイン処理
-	//while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
-	//
-	//	ScreenFlip();		//裏画面と表画面の入替
-	//	ClearDrawScreen();	//裏画面の描画を全て消去
-	//}
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
+		//ドット描画処理
+		int color = GetColor(255, 255, 255);
+		DrawPixel(400, 300, color);
+		ScreenFlip();		//裏画面と表画面の入替
+		ClearDrawScreen();	//裏画面の描画を全て消去
+	}
 
 	DxLib_End();			// ＤＸライブラリ使用の終了処理
 
 	return 0;				// ソフトの終了 
 }
-
