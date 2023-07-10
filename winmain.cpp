1ba948fc3bf16112fd7a3661621538c1125a194c
aaaa <137864418+exellents@users.noreply.github.com>
2023-07-10 16:17:45 +0900

    同期テスト

--- winmain.cpp
+++ winmain.cpp
@@ -1,7 +1,7 @@
 ﻿#include "DxLib.h"
 
 // プログラムは WinMain から始まります
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	SetOutApplicationLogValidFlag(FALSE);//ログ無効
 	//DXライブラリの初期化処理 ここから
