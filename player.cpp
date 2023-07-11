0437078e5df6264fbeef151c97043870f0fb7168
aaaa <137864418+exellents@users.noreply.github.com>
2023-07-11 15:44:50 +0900

    a

--- player.cpp
+++ player.cpp
@@ -107,7 +107,7 @@ void updatePlayer()
 		}
 	}
 
-	if (player.nage%30>10) {
+	if (player.nage%30<=10) {
 		playerimg = LoadGraph("player_1.png");
 	}
 	if (player.nage % 30 < 20&&player.nage%30>10) {
@@ -146,6 +146,12 @@ void drawPlayer()
 	if (player.inv % 5 < 2) {
 		DrawCircle(player.x, player.y, player.r, player.color, false);
 
-		DrawGraph(player.x - 115, player.y - 60, playerimg, true);
+		if (player.nage % 30 <= 10) {
+			DrawGraph(player.x - 115, player.y - 60, playerimg, true);
+		}
+		if (player.nage % 30 > 10) {
+			DrawGraph(player.x - 45, player.y - 50, playerimg, true);
+		}
+
 	}
 }
